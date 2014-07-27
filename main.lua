168928b6a7d9fa0b392e28112a39de8b67a8c9ca
Johnny Casual <johnniecasual@gmail.com>
2014-07-27 17:54:32 +0300

    Added new buttons

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 function love.load()
-	t = 0
-
 	math.randomseed(os.time())
+
 	
 	points = 0
 	love.window.setTitle("SpeedTester")
@@ -9,13 +8,30 @@ function love.load()
 	startGame = love.graphics.newImage("start_game.png")
 	soundOn = love.graphics.newImage("sound_on.png")
 	soundOff = love.graphics.newImage("sound_off.png")
+	greenOn = love.graphics.newImage("green_on.png")
+	blueOn = love.graphics.newImage("blue_on.png")
+	yellowOn = love.graphics.newImage("yellow_on.png")
+	redOn = love.graphics.newImage("red_on.png")
 	
+	game_status = false
+	print("Ikkunan koko " .. love.graphics.getWidth() ..
+		" x " .. love.graphics.getHeight())
+
+	-- pelin sisäinen aikalaskuri
+	game_time = 0
+	-- kutsutaan game_step() funktiota kerran sekunnissa
+	game_step_time = 1.0
 	print("Peli alustettu")
 end
 
-function love.update(dt)
-	t = t + dt
+function love.update(deltaTime)
+	game_time = game_time + deltaTime
+	if game_time >= game_step_time then
+		game_time = game_time - game_step_time
+		game_step()	
+	end
 end
+
 
 function love.draw()
 	--for k,v in pairs(bubbles) do
@@ -24,6 +40,8 @@ function love.draw()
 	--love.graphics.print("Points: " .. points, 400, 575)
 	love.graphics.setColor(255, 255, 255)	
 	love.graphics.draw(background, 0 , 0)
+	
+	draw_button()
 end
 
 function love.mousepressed(x, y, button)
@@ -39,6 +57,14 @@ function love.keypressed(key)
 	else
 		print("Painettu " .. key)
 	end
+end
+
+function draw_button()
+
+end
+
+function game_step()
+	print("Askellus")
 end
 
 function love.quit()
