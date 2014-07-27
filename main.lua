edb18e94e6217bafa11efe9f1df844d8968fe304
Johnny Casual <johnniecasual@gmail.com>
2014-07-27 17:00:00 +0300

    setted title

--- main.lua
+++ main.lua
@@ -1,41 +1,27 @@
 function love.load()
 	t = 0
-	max_bubbles = 10
-	max_size = 35
-	
-	bubble_grow = 7
+
 	math.randomseed(os.time())
 	
 	points = 0
-	
-	bubbles = {}
-	for i = 1, max_bubbles do
-		bubbles[i] = bubble_reset(5 * i)
-	end		
+	love.window.setTitle("SpeedTester")
 	print("Peli alustettu")
 end
 
 function love.update(dt)
 	t = t + dt
-	for i = 1, max_bubbles do
-		bubbles[i].radius = bubbles[i].radius + bubble_grow * dt
-		if bubbles[i].radius > max_size then
-			bubbles[i] = bubble_reset(0)
-		end
-	end
 end
 
 function love.draw()
-	for k,v in pairs(bubbles) do
-		love.graphics.circle("fill", v.x, v.y, v.radius)
-	end
-	love.graphics.print("Points: " .. points, 400, 575)
+	--for k,v in pairs(bubbles) do
+		--love.graphics.circle("fill", v.x, v.y, v.radius)
+	--end
+	--love.graphics.print("Points: " .. points, 400, 575)
 end
 
 function love.mousepressed(x, y, button)
 	if button == "l" then
 		print("Painettu kohdassa " .. x .. " " .. y)
-		local hit = bubble_isInside(x,y)
 	end
 	return x,y
 end
