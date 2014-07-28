81bbb79cb46e597aab6095634e00a3b76deb6080
Johnny Casual <johnniecasual@gmail.com>
2014-07-28 19:21:47 +0300

    - Created functions to change the level-info 	- Created a method to track mouse-events on three buttons 	(start-game, sound and settings) 	- Setted game-font for displaying the score info

--- main.lua
+++ main.lua
@@ -4,7 +4,14 @@ function love.load()
 	screenHeight = love.graphics.getHeight()
 
 	points = 0
+	max_levels = 6
+	current_level = 9
+	
 	love.window.setTitle("SpeedTester")
+	
+	--pelin fontti
+	font = love.graphics.newFont("Let's go Digital Regular.ttf", 44)
+	love.graphics.setFont(font)
 	
 	--Pelin kuvat
 	background = love.graphics.newImage("speed_test_bg.png")
@@ -15,6 +22,7 @@ function love.load()
 	blueOn = love.graphics.newImage("blue_on.png")
 	yellowOn = love.graphics.newImage("yellow_on.png")
 	redOn = love.graphics.newImage("red_on.png")
+	settings = love.graphics.newImage("settings.png")
 	
 	--Pelin äänet
 	blue_sound = love.audio.newSource("blue.wav", "static")
@@ -29,7 +37,9 @@ function love.load()
 	--img_y = 80
 	
 	game_status = false
-	sound = true
+	sound_is_on = true
+	sound_icon = soundOn
+	
 	print("Ikkunan koko " .. love.graphics.getWidth() ..
 		" x " .. love.graphics.getHeight())
 
@@ -56,14 +66,33 @@ function love.draw()
 	--love.graphics.print("Points: " .. points, 400, 575)
 	love.graphics.setColor(255, 255, 255)	
 	love.graphics.draw(background, 0 , 0)
-	love.graphics.draw(startGame, 45, 71) --45, 630
-	love.graphics.draw(change_soundMode(), 645, 57)
+	love.graphics.draw(startGame, 45, 75) --45, 630
+	love.graphics.draw(sound_icon, 635, 75)
+	love.graphics. draw(settings, 703, 75)
 	draw_button()
+	love.graphics.setColor(255, 2 , 9)
+	love.graphics.print("0", 220, 295) --nykyiset pisteet
+	love.graphics.print("0", 588, 295) --piste ennätys
+	level_info()
 end
 
 function love.mousepressed(x, y, button)
+	local player_hit_start = { x = 45, y = 71, width = 152, height = 40 } 
+	local setting_buttons = {}
+	local sound = { x = 635, y = 75, width = 48, height = 48 }
+	local display_settings = { x = 703, y = 75, width = 48, height = 48 }
+	
 	if button == "l" then
-		print("Painettu kohdassa " .. x .. " " .. y)
+		if x > player_hit_start.x and y > player_hit_start.y and x < player_hit_start.x + player_hit_start.width and y < player_hit_start.y + player_hit_start.height then
+			print("Painettu start game")
+		elseif x > sound.x and y > sound.y and x < sound.x + sound.width and y < sound.y + sound.height then
+				print("Painettu sound buttonia")
+				change_soundMode()
+		elseif x > display_settings.x and y > display_settings.y and x < display_settings.x + display_settings.width and y < display_settings.y + display_settings.height then
+			print("Painettu settings buttonia")
+		else
+			print("Painettu kohdassa " .. x .. " " .. y)
+		end	
 	end
 	return x,y
 end
@@ -81,11 +110,35 @@ function draw_button()
 end
 
 function change_soundMode()
-	if sound then
-		return soundOn
+	if sound_is_on == true then
+		sound_is_on = false
+		sound_icon = soundOff
+		print("Sound is off")
 	else
-		return soundOff
+		sound_is_on = true
+		sound_icon = soundOn
+		print("Sound is on")
 	end
+end
+
+function game_start()
+	love.graphics.circle()
+end
+
+function level_info()
+	local display_level = {  }
+	love.graphics.setColor(218, 209, 59)
+	local level_x = 189
+	local level_y = 190
+	local level_radius = 20
+	
+	
+	for i = 1, current_level do
+		local l = display_level[i]
+		display_level[i] = love.graphics.circle("fill", level_x, level_y, level_radius)
+		level_x = level_x + 50
+	end
+	--love.graphics.circle("fill", 189, 190, 20)
 end
 
 function game_step()
